612a9326eabb6b9a5279e059c5dd1de561e94fdb
Justice Ogbonna <ebesco@yahoo.com>
2022-03-05 20:05:49 -0800

    Organize and document the gulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,43 +10,50 @@ import decache from 'decache';
 import Jimp from 'jimp';
 import puppeteer from 'puppeteer';
 
-const backgroundScript = ['src/background-script/background.ts'];
-const contentScript = ['src/content-script/content-script.ts'];
-const popupScript = [];
-const testSpecs = ['spec/**/*.ts'];
-const assets = ['assets/**/*'];
+// The directory where generated extension files are placed.
+// You can load the extension directory from the directory via "Load unpacked".
 const outDir = './extension';
-const originalIconPath = 'assets/images/icon.png'; // png scale better than jpeg for resizing purposes.
 
+// Compile and bundle a set of TypeScript files into one Js file.
+const compileTs = (srcFiles, outputFile) => {
+    return browserify()
+        .add(srcFiles)
+        .plugin(tsify, { noImplicitAny: true, target: 'es6' })
+        .bundle()
+        .on('error', (err) => { console.error(err) })
+        .pipe(source(outputFile))
+        .pipe(gulp.dest(outDir))
+}
+
+// Background Script
+const backgroundScript = ['src/background-script/background.ts'];
 const compileBackgroundScript = () => {
-    return browserify()
-        .add(backgroundScript)
-        .plugin(tsify, { noImplicitAny: true, target: 'es6' })
-        .bundle()
-        .on('error', (err) => { console.error(err) })
-        .pipe(source('background.js'))
-        .pipe(gulp.dest(outDir))
+    return compileTs(backgroundScript, 'background.js');
 }
+const watchBackgroundScript = () => {
+    gulp.watch(backgroundScript, gulp.parallel(compileBackgroundScript));
+}
+
+// Content Script
+const contentScript = ['src/content-script/content-script.ts'];
 const compileContentScript = () => {
-    return browserify()
-        .add(contentScript)
-        .plugin(tsify, { noImplicitAny: true, target: 'es6' })
-        .bundle()
-        .on('error', (err) => { console.error(err) })
-        .pipe(source('content-script.js'))
-        .pipe(gulp.dest(outDir))
+    return compileTs(contentScript, 'content-script.js');
+}
+const watchContentScript = () => {
+    gulp.watch(contentScript, gulp.parallel(compileContentScript));
 }
 
+// Popup
+const popupScript = [];
 const compilePopupScript = () => {
-    return browserify()
-        .add(popupScript)
-        .plugin(tsify, { noImplicitAny: true, target: 'es6' })
-        .bundle()
-        .on('error', (err) => { console.error(err) })
-        .pipe(source('popup.js'))
-        .pipe(gulp.dest(outDir))
+    return compileTs(popupScript, 'popup.js');
+}
+const watchPopupScript = () => {
+    gulp.watch(popupScript, gulp.parallel(compilePopupScript));
 }
 
+// Tests
+const testSpecs = ['spec/**/*.ts'];
 const compileTests = () => {
     return gulp.src(testSpecs)
         .pipe(ts({
@@ -54,24 +61,37 @@ const compileTests = () => {
         }))
         .pipe(gulp.dest('spec'));
 }
-
-const watchBackgroundScript = () => {
-    gulp.watch(backgroundScript, gulp.parallel(compileBackgroundScript));
+const runTest = () => {
+    return new Promise((resolve, reject) => {
+        const jasmine = new Jasmine();
+        jasmine.loadConfig({
+            spec_files: ['spec/**/*.js'],
+            random: false,
+        });
+        jasmine.onComplete(passed => {
+            // multiple execute calls on jasmine env errors. See https://github.com/jasmine/jasmine/issues/1231#issuecomment-26404527
+            jasmine.specFiles.forEach(f => decache(f));
+            resolve();
+        });
+        jasmine.execute();
+    });
+}
+const test = gulp.series(compileTests, runTest);
+const watchTests = () => {
+    return gulp.watch(testSpecs, test);
 }
 
-const watchContentScript = () => {
-    gulp.watch(contentScript, gulp.parallel(compileContentScript));
+// Assets
+const assets = ['assets/**/*'];
+const originalIconPath = 'assets/images/icon.png'; // png scale better than jpeg for resizing purposes.
+const copyAssets = () => {
+    return gulp.src(assets)
+        .pipe(gulp.dest(outDir));
 }
-
-const watchPopupScript = () => {
-    gulp.watch(popupScript, gulp.parallel(compilePopupScript));
-}
-
 const watchAssets = () => {
     gulp.watch(assets, copyAssets);
 }
-
-export const generateIcons = () => {
+const generateIcons = () => {
     return new Promise((resolve, reject) => {
         Jimp.read(originalIconPath, (err, icon) => {
             if (err) {
@@ -91,46 +111,15 @@ export const generateIcons = () => {
     });
 }
 
-export const copyAssets = () => {
-    return gulp.src(assets)
-        .pipe(gulp.dest(outDir));
-}
-
-export const watchTests = () => {
-    return gulp.watch(testSpecs, gulp.series(compileTests, runTest));
-}
-
-export const runTest = () => {
-    return new Promise((resolve, reject) => {
-        const jasmine = new Jasmine();
-        jasmine.loadConfig({
-            spec_files: ['spec/**/*.js'],
-            random: false,
-        });
-        jasmine.onComplete(passed => {
-            // multiple execute calls on jasmine env errors. See https://github.com/jasmine/jasmine/issues/1231#issuecomment-26404527
-            jasmine.specFiles.forEach(f => decache(f));
-            resolve();
-        });
-        jasmine.execute();
-    });
-}
-
-export const clean = () => del([outDir]);
-clean.description = 'clean the output directory'
-
-export const build = gulp.parallel(copyAssets, compileBackgroundScript, compileContentScript, compilePopupScript);
-build.description = 'compile all sources'
-
-export const test = gulp.series(compileTests, runTest);
-
-export const pack = gulp.series(clean, build, () => {
+// Packaging
+const clean = () => del([outDir]);
+const build = gulp.series(clean, gulp.parallel(copyAssets, compileBackgroundScript, compileContentScript, compilePopupScript));
+const pack = gulp.series(build, () => {
     return gulp.src('extension/*')
         .pipe(zip('extension.zip'))
         .pipe(gulp.dest('dist'))
-})
-
-export const chromeDemo = () => {
+});
+const launchChrome = () => {
     puppeteer.launch({
         headless: false,
         ignoreDefaultArgs: ["--disable-extensions", "--enable-automation"],
@@ -139,8 +128,30 @@ export const chromeDemo = () => {
             `--load-extension=${process.env.PWD}/extension`,
         ]
     });
-}
+};
 
-const defaultTask = gulp.series(clean, build, gulp.parallel(watchBackgroundScript, watchContentScript, watchAssets, watchPopupScript))
-defaultTask.description = 'start watching for changes to all source'
-export default defaultTask
+// Exported tasks
+// These can be invoked by running: gulp <task>
+export const Build = build;
+Build.description = "Clean output directory and re-generate extension files from source.";
+
+export const Test = test;
+Test.description = "Compile and run all tests in the /spec directory";
+
+export const WatchTests = watchTests;
+WatchTests.description = "Watch changes in test files and re-run tests";
+
+export const ChromeDemo = gulp.series(build, launchChrome);
+ChromeDemo.description = "Start a chrome instance with the extension installed";
+
+export const Pack = pack;
+Pack.description = "Create a .zip file that can be uploaded to extension stores (Chrome or Firefox)";
+
+export const GenerateIcons = generateIcons;
+GenerateIcons.description = "Use assets/images/icon.png to generate the set of required icons";
+
+export const DefaultTask = gulp.series(build, gulp.parallel(watchBackgroundScript, watchContentScript, watchAssets, watchPopupScript))
+DefaultTask.description = 'Generate extension files, watch for changes and regenerate'
+
+// The default task is what runs when you simply run `gulp` without any arguments.
+export default DefaultTask;
