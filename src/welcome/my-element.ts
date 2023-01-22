077be6ab8c6ccc35b1228f75fcd902d3b6173164
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-01-21 20:38:29 -0800

    Add polyfill for custom elements in content scripts

--- src/welcome/my-element.ts
+++ src/welcome/my-element.ts
@@ -1,3 +1,5 @@
+
+import "@webcomponents/webcomponentsjs"; // Polyfill for customElements in content scripts, https://stackoverflow.com/q/42800035.
 import {LitElement, css, html} from 'lit';
 import {customElement} from 'lit/decorators.js';
 
