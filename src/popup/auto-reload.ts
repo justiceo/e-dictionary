29d40cfe99067262dafd6621c500eea84f51bd1f
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-02-22 16:40:10 -0800

    Update xtension refs to dictionary

--- src/popup/auto-reload.ts
+++ src/popup/auto-reload.ts
@@ -8,7 +8,7 @@
  * 2. Have the gulp tasks ping the extension to reload itself - https://stackoverflow.com/a/19866346. This is an overkill.
  */
 const p = document.createElement("p");
-p.innerHTML = "<button id=\"reload-extension\">Reload xtension</button>";
+p.innerHTML = "<button id=\"reload-extension\">Reload Extension</button>";
 document.querySelector("body").appendChild(p);
 document.querySelector("#reload-extension").addEventListener(
     "click",
